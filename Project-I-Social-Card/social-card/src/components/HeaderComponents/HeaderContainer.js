e8a06e41d106114c3463a333ca4f9471d9a81734
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-02-11 19:37:18 +0000

    MVP: Removed eslint alert

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Header.css';
-// import HeaderTitle from './HeaderTitle';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderContent from './HeaderContent';
 
