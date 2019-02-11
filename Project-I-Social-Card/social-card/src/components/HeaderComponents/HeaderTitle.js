41056d39d2e6273b36656192d24ddb57b2013fe7
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-02-11 12:27:30 +0000

    MVP: Created Header container component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  <div className="header-title">
+    <h4>Lambda School</h4>
+    <span className="handleAndTimestamp">@LambdaSchool • 26 jan</span>
+  </div>
+};
+
+export default HeaderTitle;
