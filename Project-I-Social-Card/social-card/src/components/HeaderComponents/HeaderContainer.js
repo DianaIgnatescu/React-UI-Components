01e003def267e2b2602ca177260716ab72577b67
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-02-11 19:32:59 +0000

    MVP: Fixed escaping alert

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import HeaderTitle from './HeaderTitle';
+// import HeaderTitle from './HeaderTitle';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderContent from './HeaderContent';
 
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -5,7 +5,11 @@ import HeaderTitle from './HeaderTitle';
 const HeaderContent = () => (
   <div className="header-content">
     <HeaderTitle />
-    <p>Let's learn Rect by building simple interfaces with components. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!</p>
+    <p>
+      Let&apos;s learn Rect by building simple interfaces with components. Don&apos;t try to
+      overthink it, just keep it simple and have fun. Once you feel comfortable using components
+      you are well on your way to mastering React!
+    </p>
   </div>
 );
 
